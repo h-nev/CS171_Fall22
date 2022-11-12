ad45239aef47116d55be57a92251984705adce70
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-12 01:08:47 -0500

    Added final visualization svg placeholder, footer for contact info / citing, and additional formatting

--- js/main.js
+++ js/main.js
@@ -5,5 +5,6 @@ let placeHolder = [1,2,3];
 // Instantiate the visualizations
 let countryBubbles = new BeeSwarm('vis-1', placeHolder);
 let fancyTimeSeries = new TimeSeries('vis-2', placeHolder);
+let charityBubbles = new Charity('vis-3', placeHolder);
 
 // Helper functions (parsers, agnostic data cleaning tools)

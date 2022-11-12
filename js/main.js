0a4718f1c0ee4903da26fabdec03c4f4300f1c72
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-11 23:27:51 -0500

    Added base code to draw the svg for the first visualization and started implementing Bootstrap grids

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Read in the data 
+placeHolder = [1, 2, 3];
+
+// Create the visualization instances
+let countryBubbles = new BeeSwarm('vis-1', placeHolder);
+
+// Helper functions (parsers, common agnostic data cleaning tasks, ect)

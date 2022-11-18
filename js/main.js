934d6f6c8be82ed9c4e9046fc05c8d5801da49d8
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-17 19:48:02 -0500

    Load award data, svg size calculations, streamline
    - Loaded the csv using d3, changed passed data to Award object to real
      data
    - Outsourced pre-processing of the data such as naming and numeric
      transformation
    - Outsourced mathematical calculations for svg height
    - Depending on display size, more awards can fit in the row, so
      re-calculate the max number per row and number of rows required to fit
      all 416 awards taking into account vertical and horizontal spacing

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ let promises = [
     d3.csv("data/billboard-top100-filtered.csv"),
     d3.csv("data/dolly-parton-released-songs.csv"),
     d3.csv("data/charts_wrangled.csv"),
+    d3.csv("data/dolly-wiki-awards.csv")
 ];
 
 Promise.all(promises)
@@ -32,11 +33,12 @@ function initMainPage(dataArray) {
             date_short: dateParser(d.date_short)
         };
     });
+
     // Instantiate the visualizations
     countryBubbles_a = new BubbleVis('vis-1a', dataArray[0], []);
     countryBubbles_b = new BubbleVis('vis-1b', dataArray[0], dataArray[1]);
     fancyTimeSeries = new TimeSeries('vis-2', chartsData);
-    awardsWon = new Awards('vis-3', placeHolder);
+    awardsWon = new Awards('vis-3', dataArray[3]);
     charityBubbles = new Charity('vis-4', placeHolder);
 }
 

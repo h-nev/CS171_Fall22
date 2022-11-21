ef78a4bf92fca714e80971959d188ade8b042870
Hope Neveux <hopeneveux@g.harvard.edu>
2022-11-21 18:00:45 -0500

    Highly unstable linear dendrogram

--- js/Dendrogram.js
+++ js/Dendrogram.js
@@ -50,11 +50,62 @@ class Dendrogram{
     wrangleData(){
         let vis = this;
 
+        // Rollup to count number of times collaborated by person and cast to dictionary, set that as the children for hierarchy purposes
+        vis.displayData = {
+            name: 'Dolly Parton',
+            children: Array.from(d3.rollup(vis.baseData, d => d.length, d => d.Connection), ([name, value]) => ({name, value}))};
+
+        console.log(vis.displayData)
+
+    
         vis.updateVis();
     }
 
     updateVis(){
         let vis = this;
+
+        vis.dendro = d3.tree()
+            .size([vis.height, vis.width]);
+            
+        let nodes = d3.hierarchy(vis.displayData, d => d.children);
+        nodes = vis.dendro(nodes);
+
+        console.log(nodes)
+
+        vis.node = vis.svg.selectAll('.node')
+            .data(nodes.descendants())
+            .enter()
+            .append('g')
+            .attr('class', d => 'node' + (d.children ? ' node--internal' : ' node--leaf'))
+            .attr('transform', d => `translate(${d.y}, ${d.x})`);
+
+        vis.edge = vis.svg.selectAll('.edge')
+            .data(nodes.descendants().slice(1))
+            .enter()
+            .append('g')
+            .attr('stroke', '#ffffff')
+            .attr('stroke-width', d => d.value)
+            .attr('d', d => {
+                return "M" + d.y + "," + d.x
+                + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+                + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+                + " " + d.parent.y + "," + d.parent.x;
+            });
+
+        vis.node.append('circle')
+           .attr('r', 20)
+           .attr('stroke', 'black')
+           .attr('fill', '#ffffff');
+           
+        vis.node.append('text')
+            .attr('dy', '.35em')
+            .attr("x", d => d.children ? (d.data.value + 5) * -1 :
+               d.data.value + 5)
+            .attr("y", d => d.children && d.depth !== 0 ?
+               -(d.data.value + 5) : d)
+            .style("text-anchor", d => d.children ? "end" : "start")
+            .text(d => d.data.name);
+
 
     }
 
